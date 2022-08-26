f3afda10e00780d1fed45f35df4b34387b15254a
hassanabdellatif <hassanahmmoh@gmail.com>
2022-08-26 03:30:28 +0200

    first commit

---
+++ lib/core/blocs/app/app_states.dart
@@ -0,0 +1,14 @@
+part of 'app_cubit.dart';
+
+@immutable
+abstract class AppStates {}
+
+class AppInitial extends AppStates {}
+
+class CurrentWeatherDataLoaded extends AppStates {}
+
+class FiveDayWeatherForecastDataLoaded extends AppStates {}
+
+class ArabCitiesLoaded extends AppStates {}
+
+class AppThemeModeChangeState extends AppStates {}
